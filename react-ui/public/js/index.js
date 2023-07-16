1309294c06933ec7995f92c5300dac8d5df35708
João Victor F. Arruda <71611166+joaoarruda101@users.noreply.github.com>
2023-07-16 13:33:35 -0300

    Mascara de telefone

--- react-ui/public/js/index.js
+++ react-ui/public/js/index.js
@@ -114,3 +114,22 @@ $(document).ready(function () {
   });
 });
 
+
+
+jQuery(document).ready(function ($) {
+  $('.inputPreco').mask('000.000.000.000.000,00', { reverse: true });
+  jQuery("input.telefone")
+      .mask("(99) 00000-0000")
+      .focusout(function (event) {
+          var target, phone, element;
+          target = (event.currentTarget) ? event.currentTarget : event.srcElement;
+          phone = target.value.replace(/\D/g, '');
+          element = $(target);
+          element.unmask();
+          if (phone.length > 10) {
+              element.mask("(99) 00000-0000");
+          } else {
+              element.mask("(99) 00000-0000");
+          }
+      });
+});
