b40cdfb51faecebb3cb366b81a42449e09c44321
João Victor F. Arruda <71611166+joaoarruda101@users.noreply.github.com>
2023-07-15 23:53:02 -0300

    Somando produtos do cardapio e selecionando no pedido

--- react-ui/public/js/index.js
+++ react-ui/public/js/index.js
@@ -1,15 +1,15 @@
 var $doc = $('html, body');
-$('.scrollSuave').click(function() {
-    $doc.animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
-    }, 500);
-    return false;
+$('.scrollSuave').click(function () {
+  $doc.animate({
+    scrollTop: $($.attr(this, 'href')).offset().top
+  }, 500);
+  return false;
 });
 
 $("#menu").click(function (e) {
-	$(".nav-menu").toggleClass('is-open');
-	$(".overlay").toggleClass('is-visible');
-	$(this).toggleClass('open');
+  $(".nav-menu").toggleClass('is-open');
+  $(".overlay").toggleClass('is-visible');
+  $(this).toggleClass('open');
 });
 
 
@@ -18,19 +18,51 @@ $(".button-collapse").click(function () {
   $(".side-nav").toggleClass('side-nav-open');
   $(".button-collapse").toggleClass('anda');
   $("#main_menu").toggleClass('roda');
-  
+
 });
 
 $(".topo-fixo > a").click(function (e) {
-    $(this).closest('.side-nav').toggleClass("side-nav-open");
-    e.stopPropagation();
+  $(this).closest('.side-nav').toggleClass("side-nav-open");
+  e.stopPropagation();
 });
 
-$(document).on('click', function (e) {    
-    if (!$(e.target).closest('.side-nav-open').length ||
-      $(e.target).closest('li').length){
+$(document).on('click', function (e) {
+  if (!$(e.target).closest('.side-nav-open').length ||
+    $(e.target).closest('li').length) {
 
-      $('ul').removeClass('side-nav-open');
-      $('.button-collapse').removeClass('anda');
-    } 
+    $('ul').removeClass('side-nav-open');
+    $('.button-collapse').removeClass('anda');
+  }
 });
+
+
+$('.incrementar').click(function () {
+
+  var paragrafo = $('#contador');
+  var texto = paragrafo.text();
+  var novoTexto = texto.replace('R$', '')
+
+  var valorProduto = $(this).val();
+
+  var numero1 = parseFloat(valorProduto);
+  var numero2 = parseFloat(novoTexto);
+
+  soma = numero1 + numero2
+
+  $('#contador').text("R$" + soma);
+});
+
+$(document).ready(function() {
+  $('.incrementar').click(function() {
+    var nomeProduto = $(this).closest('.col').find('.nomeProduto').text();
+    
+    var textoInput = $('#pedido').val();
+    console.log(textoInput);
+    
+    $('#pedido').val(textoInput + nomeProduto + " , ");
+
+  });
+});
+
+
+
